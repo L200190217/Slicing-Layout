2891610bc2999709a681a83316ad0e7f7b7d974f
L200190217 <suryasumirat678@gmail.com>
2022-02-25 15:56:06 +0700

    menambah page feature & pricing & perubahan page about

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,14 +20,18 @@
     </div>
 </section>
 
-<section class="services mt-5">
+
+
+
+
+<section class="services2 mt-5">
     <img class="bg-2" src="image/Background Gradient2.svg" alt="">
     <div class="container">
         <div class="row mt-5">
-            <div class="service col-lg-6">
-                <center>
-                    <img class="mask" src="image/Mask.png" alt="">
-                </center>
+            <div class="service2 col-lg-6">
+
+                <img class="mask" src="image/Mask.png" alt="">
+
             </div>
             <div class="service col-lg-6">
                 <h1 class="fs-4 mt-4">{{ $judul2 }}</h1>
@@ -40,5 +44,244 @@
     </div>
     </div>
 </section>
+
+
+
+
+
+
+<!-- <div class="cat">
+            <img src="image/Cat Paw.svg" style="height: 200px;">
+        </div> -->
+
+
+<!-- <div class="bacground"> -->
+<section class="portfolio2 pb-4">
+    <div class="container">
+
+        <!-- <div class="bg-3">
+            <img src="image/Rectangle 1.svg">
+        </div> -->
+
+        <div class="row text-center pt-4 mb-4">
+            <div class="col-lg-12 mt-5">
+                <h1>Bermacam Fitur Manjakan Kucingmu</h1>
+            </div>
+        </div>
+
+
+
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"><img class="mb-2 me-3" src="image/fa-code.svg" style="height: 25px;">Feature 1</h5>
+                        <p class="card-text">{{ $feature1 }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"><img class="mb-2 me-3" src="image/fa-html5.svg" style="height: 25px;">Feature 2</h5>
+                        <p class="card-text">{{ $feature2 }}</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-lg-4 col-md-6">
+                <div class="" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"><img class="mb-2 me-3" src="image/fa-cogs.svg" style="height: 25px;">Feature 3</h5>
+                        <p class="card-text">{{ $feature3 }}</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="col-lg-4 col-md-6">
+                <div class="" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"><img class="mb-2 me-3" src="image/fa-code (1).svg" style="height: 25px;">Feature 4</h5>
+                        <p class="card-text">{{ $feature4 }}</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="col-lg-4 col-md-6">
+                <div class="" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"><img class="mb-2 me-3" src="image/fa-calendar-check-o.svg" style="height: 25px;">Feature 5</h5>
+                        <p class="card-text">{{ $feature5 }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"><img class="mb-2 me-3" src="image/fa-cogs (1).svg" style="height: 25px;">Feature 6</h5>
+                        <p class="card-text">{{ $feature6 }}</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+    </div>
+</section>
+</div>
+
+
+
+<section class="portfolio2 pb-4">
+    <div class="container">
+
+        <div class="row text-center pt-4 mb-4">
+            <div class="col-lg-12 mt-5">
+                <h1>Berlangganan Sebulan Cuma Seharga Segelas Kopi</h1>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="mx-auto" style="width: 300px;">
+                    <div class="card" style="width: 18rem; border-radius: 10px;">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">STARER</h5>
+                            <center>
+                                <span class="kiri">
+                                    <sup class="rp">Rp</sup>
+                                    <sub class="angka">0</sub>
+                                </span>
+                                <span class="kanan">
+                                    <sub class="Hzx">per user</br></sup>
+                                        <sub class="Hzy">per mounth</sub>
+                                </span>
+
+                                </br><br>
+                                <p class="card-text" style="color: #5B5B7F;">{{ $berlangganan1 }}</p>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="color style=" width: 18rem;">
+                        <div class="card-body">
+                            <p class="card-title"><img class="mb-1 me-3" src="image/Check Icon.svg" style="height: 9px;">Unlimited Likes
+                            </p>
+                            <p class="card-title"><img class="mb-1 me-3" src="image/Check Icon.svg" style="height: 9px;">Unlimited Match
+                            </p>
+                            <p class="card-title"><img class="mb-1 me-3" src="image/Check Icon.svg" style="height: 9px;">See Who Likes
+                                Your Cat</p>
+                            <p class="card-title"><img class="mb-1 me-3" src="image/Check Icon.svg" style="height: 9px;">No Ads</p>
+                        </div>
+                        <center class="tombol-star text-center">
+                            <h5>Start Free Trial</h5>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="mx-auto" style="width: 300px;">
+                    <div class="card" style="width: 18rem; border-radius: 10px; background-color: #8080B2; color: #FFFFFF;">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">STANDARD</h5>
+                            <center>
+                                <span class="kiri">
+                                    <sup class="rp">Rp</sup>
+                                    <sub class="angka">45K</sub>
+                                </span>
+                                <span class="kanan">
+                                    <sub class="Hzx">per user</br></sup>
+                                        <sub class="Hzy">per mounth</sub>
+                                </span>
+                                </br><br>
+                                <p class="card-text">{{ $berlangganan1 }}</p>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="color" style="width: 18rem;">
+                        <div class="card-body">
+                            <p class="card-title"><img class="mb-1 me-3" src="image/Check Icon.svg" style="height: 9px;">Unlimited Likes</p>
+                            <p class="card-title"><img class="mb-1 me-3" src="image/Check Icon.svg" style="height: 9px;">Unlimited Match</p>
+                            <p class="card-title"><img class="mb-1 me-3" src="image/Check Icon.svg" style="height: 9px;">See Who Likes Your
+                                Cat</p>
+                            <p class="card-title"><img class="mb-1 me-3" src="image/Check Icon.svg" style="height: 9px;">No Ads</p>
+                        </div>
+                        <center class="tombol-star text-center" style="background-color: #41D8BB; color: white;">
+                            <h5>Start Free Trial</h5>
+                        </center>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-lg-4 col-md-4 offset-lg-0 offset-md-3 mb-4">
+                <div class="mx-auto" style="width: 300px;">
+                    <div class="card" style="width: 18rem; border-radius: 10px;">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">PREMIUM</h5>
+                            <center>
+                                <span class="kiri">
+                                    <sup class="rp">Rp</sup>
+                                    <sub class="angka">50K</sub>
+                                </span>
+                                <span class="kanan">
+                                    <sub class="Hzx">per user</br></sup>
+                                        <sub class="Hzy">per mounth</sub>
+                                </span>
+                                </br><br>
+                                <p class="card-text" style="color: #5B5B7F;">{{ $berlangganan1 }}</p>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="color" style="width: 18rem;">
+                        <div class="card-body">
+                            <p class="card-title"><img class="mb-1 me-3" src="image/Check Icon.svg" style="height: 9px;">Unlimited Likes</p>
+                            <p class="card-title"><img class="mb-1 me-3" src="image/Check Icon.svg" style="height: 9px;">Unlimited Match</p>
+                            <p class="card-title"><img class="mb-1 me-3" src="image/Check Icon.svg" style="height: 9px;">See Who Likes Your
+                                Cat</p>
+                            <p class="card-title"><img class="mb-1 me-3" src="image/Check Icon.svg" style="height: 9px;">No Ads</p>
+                        </div>
+                    </div>
+                    <center class="tombol-star text-center">
+                        <h5>Start Free Trial</h5>
+                    </center>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        <!-- <div class="col-lg-4 col-md-6">
+        
+        </div>
+        
+                <div class="col-lg-4 col-md-6">
+                    
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    
+                </div>
+        
+                </div> -->
+
+
+
+
+    </div>
+</section>
+</div>
+
+
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,16 @@ Route::get('/', function () {
         "title" => "Home",
         "text2" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua.",
-        "judul2" => "Apa sih, CATMATE itu?"
+        "judul2" => "Apa sih, CATMATE itu?",
+        "feature1" => "HTML layout is based on one of the most common and reliable frame-work -Bootstrap.",
+        "feature2" => "We used only time-tested technolo-gies for the best results.",
+        "feature3" => "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        "feature4" => "There are a lot of different compo-nents that will help you create the perfect look and feel for your startup.",
+        "feature5" => "Take a break from the routine and spend your time brainstorming ideas for your business, not your website.",
+        "feature6" => "Startups can save money on design and code and use those savings to develop the business.",
+
+        "berlangganan1" => "All the features you need to keep your personal files safe, accessible, and easy to share.",
+        "berlangganan2" => "All the features you need to keep your personal files safe, accessible, and easy to share.",
+        "berlangganan3" => "All the features you need to keep your personal files safe, accessible, and easy to share."
     ]);
 });
